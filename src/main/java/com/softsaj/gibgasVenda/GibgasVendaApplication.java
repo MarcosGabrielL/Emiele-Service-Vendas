49d772d584fea4010f7475ade3b204ef2b5b776d
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-03-12 16:57:25 -0300

    Initial Config Kafka

--- src/main/java/com/softsaj/gibgasVenda/GibgasVendaApplication.java
+++ src/main/java/com/softsaj/gibgasVenda/GibgasVendaApplication.java
@@ -1,7 +1,10 @@
 package com.softsaj.gibgasVenda;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class GibgasVendaApplication {
@@ -9,5 +12,12 @@ public class GibgasVendaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GibgasVendaApplication.class, args);
 	}
+        
+        @Bean
+        CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+            return args -> {
+              kafkaTemplate.send("vendas", "Message");
+            };
+        }
 
 }
