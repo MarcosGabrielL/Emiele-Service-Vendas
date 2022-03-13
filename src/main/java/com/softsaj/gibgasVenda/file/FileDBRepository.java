6831cad32c9d8f079b9463021fbdc48f42f58648
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-03-13 10:13:41 -0300

    exception transational

--- src/main/java/com/softsaj/gibgasVenda/file/FileDBRepository.java
+++ src/main/java/com/softsaj/gibgasVenda/file/FileDBRepository.java
@@ -14,8 +14,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@Transactional
 @Repository
 public interface FileDBRepository extends JpaRepository<FileDB, Long> {
     
